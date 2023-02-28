0ec6e07d4022199a8028f5a7ca3db490523851d8
CSY0810 <rktrem@mail.ustc.edu.cn>
2023-02-28 21:17:20 +0800

    first commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: CSYrk
+	> Mail: rktrem@mail.ustc.edu.cn 
+	> Created Time: 2023年02月28日 星期二 21时16分13秒
+ ************************************************************************/
+
+#include<stdio.h>
+int main(){
+	printf("Hello GitHub!");
+	return 0;
+}
+
