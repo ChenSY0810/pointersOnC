b68014e002ea3b625fce9c00af67a42a4288da40
CSY0810 <rktrem@mail.ustc.edu.cn>
2023-03-05 17:22:53 +0800

    modified:   chapter2/prac1.c

--- chapter2/prac1.c
+++ chapter2/prac1.c
@@ -9,8 +9,8 @@ int negate (int i){
 
 
 int main (){
-    printf("%d %d", increment(10), negate(10));
-    printf("%d %d", increment(0), negate(0));
-    printf("%d %d", increment(-10), negate(-10));
+    printf("%d %d\n", increment(10), negate(10));
+    printf("%d %d\n", increment(0), negate(0));
+    printf("%d %d\n", increment(-10), negate(-10));
     return 0;
 }
